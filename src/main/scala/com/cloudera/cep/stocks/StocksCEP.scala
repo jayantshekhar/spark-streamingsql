0083ba91a6378b0b627acb21f82ba26f1021a550
Jayant Shekar <jayant@user-MBPMBA-3.local>
2015-07-22 13:58:14 +0530

    adding more stock queries

--- src/main/scala/com/cloudera/cep/stocks/StocksCEP.scala
+++ src/main/scala/com/cloudera/cep/stocks/StocksCEP.scala
@@ -15,24 +15,42 @@ object StocksCEP {
 
   def main(args: Array[String]): Unit = {
 
+    // create StreamSQLContext
     val ssc = SparkConfUtil.createSparkContext(20000, "StocksCEP")
     val sc = ssc.sparkContext
 
     val streamSqlContext = new StreamSQLContext(ssc, new SQLContext(sc))
     import streamSqlContext._
 
+    // create DStream of stocks
     val stockRDD1 = sc.parallelize(1 to 100).map(i => Stock(randomSymbol(), randomPrice()))
     val stockStream1 = new ConstantInputDStream[Stock](ssc, stockRDD1)
     registerDStreamAsTable(stockStream1, "stocks")
 
-    sql("SELECT * FROM stocks").map(_.copy()).print()
+    // run queries
+    runQueries(streamSqlContext)
 
+
+    // start/stop
     ssc.start()
-    ssc.awaitTerminationOrTimeout(30 * 1000)
+    ssc.awaitTerminationOrTimeout(300 * 1000)
     ssc.stop()
   }
 
-  //
+  // run the queries
+  def runQueries(streamSqlContext : StreamSQLContext): Unit = {
+
+    // display all the stock ticks
+    //streamSqlContext.sql("SELECT * FROM stocks").map(_.copy()).print()
+
+    // display count/avg of ticks for each symbol
+    //streamSqlContext.sql("SELECT symbol, count(*), avg(price) FROM stocks group by symbol").map(_.copy()).print()
+
+    // display symbols for which there is rapid drop-off : change > 96
+    streamSqlContext.sql("SELECT symbol, avg(price), (max(price) - min(price)) as chg FROM stocks group by symbol having chg > 96.0").map(_.copy()).print()
+  }
+
+  // generate random symbol
   def randomSymbol() : String = {
     var idx = (Math.random() * 100).toInt
 
@@ -46,6 +64,7 @@ object StocksCEP {
 
   }
 
+  // generate random price
   def randomPrice() : Double = {
     var v = Math.random() * 100
 
